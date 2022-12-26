97a0a2901f9f14fad93c738f32da00759e4b5966
Melisa Çörtük <76431780+melisacortuk@users.noreply.github.com>
2022-12-26 23:57:48 +0300

    Add files via upload

---
+++ harry-potter-website/assets/js/main.js
@@ -0,0 +1,6 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.querySelector('.nav-menu');
+/*===== MENU SHOW ====*/
+document.querySelector('.nav-toggle').onclick = () => {
+  navMenu.classList.toggle('show-menu');
+}
